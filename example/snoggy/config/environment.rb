6c13db64feb3c426bc0f0c638ef683fa32439bf6
Theo <theo@iconara.net>
2010-03-30 11:43:11 +0200

    Updated the documentation

--- example/snoggy/config/environment.rb
+++ example/snoggy/config/environment.rb
@@ -3,6 +3,11 @@ RAILS_GEM_VERSION = '2.3.5' unless defined? RAILS_GEM_VERSION
 require File.join(File.dirname(__FILE__), 'boot')
 
 Rails::Initializer.run do |config|
+  # Usually you would add SNOGmetrics by adding the gem here, but in this
+  # example app I have put the code as a plugin, because it's more convenient
+  # when I code new features.
+  #config.gem 'snogmetrics'
+  
   config.frameworks -= [:active_record, :active_resource, :action_mailer]
   config.action_controller.session = {:key => '_snoggy_session', :secret => 'd08d2ef897ba8d7477bc3088dde396ac'}
 end
--- example/snoggy/config/initializers/snogmetrics.rb
+++ example/snoggy/config/initializers/snogmetrics.rb
@@ -1,5 +1,14 @@
+# This is where you configure SNOGmetrics. Just override the #kissmetrics_api_key
+# method of the module to return the API key.
 module Snogmetrics
   def kissmetrics_api_key
     'abc123'
   end
 end
+
+# If you need to load the API from a YAML file, you can do something like the
+# following. It loads the key and puts it in a local variable, then it overrides
+# the #kissmetrics_api_key method with a closure that has access to the local
+# variable 'key'.
+#key = YAML.load(ERB.new(File.read('path/to/file.yml')).result)[RAILS_ENV].symbolize_keys[:kissmetrics_api_key]
+#Snogmetrics.send(:define_method, :kissmetrics_api_key) { key }
