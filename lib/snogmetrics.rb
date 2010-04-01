577aa0bb9eb0f29dd02a1e5b2ffd387ef15dfbe3
Theo <theo@iconara.net>
2010-04-01 20:34:41 +0200

    Added API documentation

--- lib/snogmetrics.rb
+++ lib/snogmetrics.rb
@@ -1,6 +1,9 @@
 module Snogmetrics
   VERSION = '0.1.1'
-  
+
+  # Returns an instance of KissmetricsApi, which is an interface to the
+  # KISSmetrics API. It has the methods #record and #identify, which work just
+  # like the corresponding methods in the JavaScript API.
   def km
     @km_api ||= KissmetricsApi.new(kissmetrics_api_key, session, fake_km_api?)
   end
@@ -10,15 +13,30 @@ module Snogmetrics
     ''
   end
   
+  # Override this method to customize when the real API and when the stub API
+  # will be outputted.
+  def fake_km_api?
+    if defined? Rails
+      ! Rails.env.production?
+    else
+      false
+    end
+  end
+  
 private
 
   class KissmetricsApi
+    # Do not instantiate KissmetricsApi yourself, instead mix in Snogmetrics
+    # and use it's #km method to get an instance of KissmetricsApi.
     def initialize(api_key, session, fake_it)
       @session = session
       @api_key = api_key
       @fake_it = fake_it
     end
     
+    # The equivalent of the `KM.record` method of the JavaScript API. You can
+    # pass either an event name, an event name and a hash of properties, or only
+    # a hash of properties.
     def record(*args)
       raise 'Not enough arguments' if args.size == 0
       raise 'Too many arguments' if args.size > 2
@@ -26,6 +44,7 @@ private
       queue << ['record', *args]
     end
     
+    # The equivalent of the `KM.identify` method of the JavaScript API.
     def identify(identity)
       unless @session[:km_identity] == identity
         queue.delete_if { |e| e.first == 'identify' }
@@ -34,6 +53,23 @@ private
       end
     end
     
+    # Equivalent to `js(:reset => true)`, i.e. returns the JavaScript code
+    # needed to load the KISSmetrics API and send the current state, and reset
+    # the state afterwards.
+    def js!
+      js(:reset => true)
+    end
+    
+    # In most situations you want the #js! method instead of this one.
+    #
+    # Returns the JavaScript needed to send the current state to KISSmetrics.
+    # This includes the a JavaScript tag that loads the API code (or a fake API
+    # that sends events to the console if the `fake_it` parameter to #initialize
+    # is true), as well as statements that push the current state onto the 
+    # `_kmq` array.
+    #
+    # You can pass the option `:reset` to reset the state, this makes sure that
+    # subsequent calls to #js will not send events that have already been sent.
     def js(options={})
       options = {:reset => false}.merge(options)
       
@@ -52,10 +88,6 @@ private
         </script>
         JAVASCRIPT
       end
-    end
-    
-    def js!
-      js(:reset => true)
     end
   
   private
